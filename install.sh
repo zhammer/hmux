f2ed3e18f35ce4b31ea5e75ff0163c4a11da36b3
Zach Hammer <zhammer1@bloomberg.net>
2017-12-19 17:38:28 -0500

    Add install file

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Install hmux's config file by creating a symlink from
+# ~/.tmux.conf -> $GIT_DIR/.tmux.conf.
+# If there is already a ~/.tmux.conf file, save that config
+# in $GIT_DIR/old-config. (If the current ~/.tmux.conf is a
+# symlink, the symlinks target file will be copied.)
+
+GIT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+USER_TMUX_CONF=~/.tmux.conf
+HMUX_CONF="$GIT_DIR/.tmux.conf"
+
+# ~/.tmux.conf exists, and it is a symlink
+if [ -h $USER_TMUX_CONF ]; then
+    mkdir $GIT_DIR/old-config
+    TARGET_FILE=$(readlink $USER_TMUX_CONF)
+    cp $TARGET_FILE $GIT_DIR/old-config/.tmux.conf.old
+    rm -f $USER_TMUX_CONF
+# ~/.tmux.conf exists, and it is not a symlink
+elif [ -e $USER_TMUX_CONF ]; then
+    mkdir $GIT_DIR/old-config
+    mv $USER_TMUX_CONF $GIT_DIR/old-config/.tmux.conf.old
+fi
+
+ln -s /home/zhammer1/mbig/personal/hmux/.tmux.conf ~/.tmux.conf
