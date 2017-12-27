75ee4a0123a5fe2693e5fd8efaa8574b0a01c384
zhammer <zachary_hammer@brown.edu>
2017-12-27 16:02:29 -0500

    Add special case for tx ls arg in tx.sh

--- tx.sh
+++ tx.sh
@@ -5,6 +5,12 @@
 # Attach to tmux session SESSION_NAME if it exists, otherwise create a new session.
 tx () {
     SESSION_NAME=$1
+
+    if [ $SESSION_NAME == "ls" ]; then
+        tmux ls
+        return
+    fi
+
     tmux ls | grep "^${SESSION_NAME}:"
     if [ $? -ne 0 ]; then
         tmux new -s $SESSION_NAME
