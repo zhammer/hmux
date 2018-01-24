ddd1991d7e9ccfdcfaf75f959ecf2121716060da
zhammer <zachary_hammer@brown.edu>
2018-01-24 12:53:13 -0500

    Make extract_help_bindings function testable

--- help_doc_gen.py
+++ help_doc_gen.py
@@ -13,11 +13,10 @@ HELP_PAGE_HEADER = ('You have typed C-h, the help character. Type a Help option:
                     '(Type q to exit the Help command.)')
 HelpBinding = collections.namedtuple('HelpBinding', 'key command docstring')
 
-def extract_conf_file_help_bindings(conf_file):
-    """Extract all lines in our config that are help key bindings and return them as aa list of
-    HelpBinding namedtuples."""
-    with open(conf_file) as lines:
-        return [HelpBinding(*re.match(CONF_HELP_BINDING_RE, line).groups()) for line in lines
+def extract_help_bindings(tmux_conf_lines):
+    """Extract all lines in an iterable of tmux config lines that are help key bindings and return
+    them as a list of HelpBinding namedtuples."""
+    return [HelpBinding(*re.match(CONF_HELP_BINDING_RE, line).groups()) for line in tmux_conf_lines
                 if re.match(CONF_HELP_BINDING_RE, line)]
 
 def sort_help_bindings_by_key(help_bindings):
@@ -34,8 +33,10 @@ def format_help_binding(help_binding):
 
 def main():
     """Main"""
-    conf_file = sys.argv[1]
-    help_bindings = extract_conf_file_help_bindings(conf_file)
+    with open(sys.argv[1]) as tmux_conf_file:
+        tmux_conf_lines = [line for line in tmux_conf_file]
+
+    help_bindings = extract_help_bindings(tmux_conf_lines)
     help_bindings = sort_help_bindings_by_key(help_bindings)
 
     print(HELP_PAGE_HEADER + '\n')
